73ec9d7f827f63568938982cfe847699fcb33277
Radim Ciz <radimciz@yahoo.com>
2025-06-02 19:54:08 +0200

    POM hotel base test

---
+++ src/main/java/selenium/POM/TestSuites/HotelTests.java
@@ -0,0 +1,76 @@
+package selenium.POM.TestSuites;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import selenium.POM.Pages.FormPage;
+
+import java.io.File;
+
+public class HotelTests {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        openOnSecondScreen_ForLecturerDemoOnly();
+        driver.manage().window().maximize();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+    }
+
+    @Test
+    public void registerUserTest() {
+    }
+
+
+
+    @AfterEach
+    public void tearDown() {
+        waitBeforeClosing_ForLecturerDemoOnly();
+        if (driver != null)
+            driver.quit();
+    }
+
+    // *********************** helper methods *************************
+
+    private void openOnSecondScreen_ForLecturerDemoOnly() {
+        if (IsOnLecturerPc()) {
+            driver.manage().window().setPosition(new Point(-2000, 0));
+        }
+    }
+
+    private void waitBeforeClosing_ForLecturerDemoOnly() {
+        if (IsOnLecturerPc()) {
+            try {
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void wait2Sec_ForLecturerDemoOnly() {
+        if (IsOnLecturerPc()) {
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean IsOnLecturerPc() {
+        boolean inLecturerPc = false;
+        File file = new File(System.getProperty("user.dir") + File.separator + "runInLecturerPc.txt");
+        if (file.exists()) {
+            inLecturerPc = true;
+        }
+        return inLecturerPc;
+    }
+
+}
